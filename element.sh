3739cb9fc07c1534f563fd7d20e4e3e0dd91f3e7
Eugene <genewong2016@gmail.com>
2023-01-05 13:39:15 +0800

    Create element.sh

---
+++ element.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+PSQL="psql --csv --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1;")
+  else
+    ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1';")
+  fi
+
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    IFS=',' read ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< $ELEMENT
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
+fi
